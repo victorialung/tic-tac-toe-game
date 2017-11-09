e28901c4cf59678e93d6ff913bb0fa6dedace56f
victorialung <vtr.lung@outlook.es>
2017-11-09 04:36:30 -0500

    agregando interactividad al programa

---
+++ js/app.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    var board= document.querySelector(".board");
+
+    board.addEventListener("click",addX);
+    };
+
+    var centinel = true;
+
+    function addX(event){
+        if(event.target.matches("td") && event.target.textContent===""){
+            if(centinel){
+                event.target.textContent ="X";
+                centinel=false;
+            }else{
+                event.target.textContent ="O";
+                centinel=!centinel;
+            }
+        }
+    }
